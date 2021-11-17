a6debae034f387f3650018bffb86049d16a56de3
bokdelajungla <bokdelajungla@hotmail.com>
2021-11-17 19:29:56 +0100

    First Commit

---
+++ src/main/java/es/serviciosrest/PspA3RestApplication.java
@@ -0,0 +1,13 @@
+package es.serviciosrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PspA3RestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PspA3RestApplication.class, args);
+	}
+
+}
